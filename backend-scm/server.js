54cc6c86232cbbc3d229c3729fab05ae16ea75d5
Pedro Ramos <ramuspedro@gmail.com>
2020-08-11 23:06:31 -0300

    cors enabled

--- backend-scm/server.js
+++ backend-scm/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const github = require("./api/github");
+var cors = require('cors');
 
 const app = express();
 
@@ -8,6 +9,7 @@ const apiPrefix = "/api/scm"
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(`${apiPrefix}/github`, github);
 app.get(`${apiPrefix}/health`, (req, res) => res.json({ msg: "Health OK" }));
